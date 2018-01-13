cb2719f0be01d172da7858693990464ac95763ec
Robin Soubry <robinsoubry@Robins-MacBook-Pro.local>
2018-01-13 22:01:46 +0100

    add routes and pseudo-code

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,3 +1,44 @@
 get '/' do
   "Hello World!"
 end
+
+# CONTACTS RESTful actions
+
+# index
+get '/contacts' do
+  'This is the contacts index action'
+end
+
+# new
+get '/contacts/new' do
+  'This is the contacts new action'
+end
+
+# create
+post '/contacts' do
+  'This is the contacts create action'
+end
+
+# show
+get '/contacts/:id' do
+  'This is the contacts show action'
+end
+
+# edit
+get '/contacts/:id/edit' do
+  'This is the contacts edit action'
+end
+
+# update
+patch '/contacts/:id' do
+  'This is the contacts update action'
+end
+
+put '/contacts/:id ' do
+  'This is the contacts update action'
+end
+
+# delete
+delete '/contacts/:id' do
+  'This is the contacts deletion action'
+end
